8357ef7e29ccbeb29df06cc61f61796d2db746e6
sinedotwav <sinedotwav-e@users.noreply.github.com>
2023-05-04 12:07:05 +1200

    Update pizzabot.py

---
+++ pizzabot.py
@@ -0,0 +1,13 @@
+# pizzabot program
+import random
+from random import randint
+
+
+# names to be randomly selected
+names = ["Doug", "Douglas", "Dougington", "Dougie", "Duggie", "Doogie", "Dougalasa", "Dùbhghlas", "Thrangott the Devourer", "Gab"]
+
+num = randint(0,9)
+
+name = (names[num])
+
+print(name)
