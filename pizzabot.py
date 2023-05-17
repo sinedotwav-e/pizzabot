4cb6756d97fb2b460008cfa23acd477780060d09
sinedotwav <55688664+sinedotwav-e@users.noreply.github.com>
2023-05-17 22:24:19 +1200

    Added welcome_v1.py as a component
    Implemented welcome_v1.py as a function of pizzabot.py

--- pizzabot.py
+++ pizzabot.py
@@ -3,4 +3,34 @@ import random
 from random import randint
 
 # names for the random name generator
-names = ["Doug", "Douglas", "Dougington", "Dougie", "Duggie", "Doogie", "Dougalasa", "Dùbhghlas", "Thrangott the Devourer", "Gab"]
+names = ["Doug", "Douglas", "Dougington", "Dougie", "Duggie", "Doogie", "Dougalasa", "Dùbhghlas", "Thrangott the Devourer", "Gab", "Doug Doug"]
+
+# welcome screen
+def welcome():
+  '''
+  Purpose: To generate a random name from a list and print out a welcome message
+  Parameters: None
+  Returns: None
+  '''
+
+  num = randint(0,10)
+  name = (names[num])
+
+  # welcome message with random name
+  print("· ⋆ ★ Welcome to Doug's Dream Pizza ★ ⋆ ·")
+  print("Hey, my name is", name + ",")
+  print("and I will help you order your pizza.")
+
+# delivery and pick-up menu
+def delpick():
+  print("nothing")
+
+def main():
+  '''
+  Purpose: Run all functions
+  Parameters: None
+  Returns: None
+  '''
+  welcome()
+
+main()
