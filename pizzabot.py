138bacff4035997f887a7f79aa7ab19e8d621333
sinedotwav <55688664+sinedotwav-e@users.noreply.github.com>
2023-05-18 21:54:47 +1200

    Implemented deliverypickup_v5.py as a component
    Added deliverypickup_v5.py as a component of pizzabot.py

--- pizzabot.py
+++ pizzabot.py
@@ -17,10 +17,37 @@ def welcome():
   print("· ⋆ ★ Welcome to Doug's Dream Pizza ★ ⋆ ·")
   print("Hey, my name is", name + ",")
   print("and I will help you order your pizza.")
+  print("")
 
 # delivery and pick-up menu
-def delpick():
-  print("nothing")
+def deliverypickup():
+  print("Would you like to order for pick-up or delivery?")
+  print("Enter 1 for pick-up")
+  print("Enter 2 for delivery (will incur a $3 surcharge)")
+
+  # while loop to avoid invalid inputs
+  while True:
+    try:
+      delpick = int(input("Enter number here > "))
+      if delpick >= 1 and delpick <= 2:
+
+        if delpick == 1:
+          print("Pickup!")
+          break
+
+        elif delpick == 2:
+          print("Delivery!")
+          break
+
+      else:
+        print("Number must be 1 or 2, please try again")
+        print("Enter 1 for pick-up")
+        print("Enter 2 for delivery (will incur a $3 surcharge)")
+
+    except ValueError:
+      print("Invalid input, please try again")
+      print("Enter 1 for pick-up")
+      print("Enter 2 for delivery (will incur a $3 surcharge)")
 
 # runs all functions
 def main():
@@ -30,5 +57,6 @@ def main():
   Returns: None
   '''
   welcome()
+  deliverypickup()
 
 main()
