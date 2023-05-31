2b5c0e17430d469929f67d04f54aaedd0a2aa9b5
sinedotwav <sinedotwav-e@users.noreply.github.com>
2023-05-31 15:02:49 +1200

    Created delinfo_v1.py
    Created delinfo_v1.py and added lines of code to it. Asks for your name and number as well as your address.
    
    Code taken from pickinfo_v4.py

---
+++ components/delinfo/delinfo_v1.py
@@ -0,0 +1,43 @@
+# bugs
+
+# information to be collected after the delivery option is selected
+
+# customer details dictionary
+userdetails = {}
+
+# function for blank inputs
+def notblank(query):
+  valid = False
+  while not valid:
+    response = input(query)
+
+    if response != "":
+      print("")
+      return response
+  
+    else:
+      print("")
+      print("Entry cannot be blank, try again.")
+
+# basic intructions
+query = ("Please enter your name > ")
+userdetails['name'] = notblank(query)
+
+query = ("Please enter your phone number > ")
+userdetails['phone'] = notblank(query)
+
+query = ("Please enter your house number > ")
+userdetails['house'] = notblank(query)
+
+query = ("Please enter your street name > ")
+userdetails['street'] = notblank(query)
+
+query = ("Please enter your suburb > ")
+userdetails['suburb'] = notblank(query)
+
+# print user details
+print(userdetails['name'])
+print(userdetails['phone'])
+print(userdetails['house'])
+print(userdetails['street'])
+print(userdetails['suburb'])
